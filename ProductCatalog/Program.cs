7ec495c94286f44f06ee0c1a89273a00bddd971f
Son-JuHwan <sonjuhawn@gmail.com>
2026-03-04 14:27:01 +0900

    문제 2 진행중

--- ProductCatalog/Program.cs
+++ ProductCatalog/Program.cs
@@ -2,3 +2,18 @@ using System;
 
 // README.md를 읽고 코드를 작성하세요.
 Console.WriteLine("코드를 작성하세요.");
+
+class Product
+{
+    public string Name;
+    public int Price;
+
+    public override string ToString()
+    {
+        return $"[{Name}] - ₩[{Price:N0}";
+    }
+    public virtual string GetDescription()
+    {
+
+    }
+}
